354d4f16d7271cf660c9eac9176a0b3fb1701398
Vighanesh Sharma <vighaneshs@Vighaneshs-MacBook-Pro.local>
2025-11-26 21:58:16 -0600

    Adding PERSON table

--- schema/tables/01_person.sql
+++ schema/tables/01_person.sql
@@ -1,7 +1,7 @@
 USE jungle_library_db;
 
 CREATE TABLE PERSON (
-    Person_ID CHAR(4) PRIMARY KEY,
+    Person_ID CHAR(4) PRIMARY KEY CHECK (Person_ID REGEXP '^P[0-9]{3}$'),
     Fname VARCHAR(50) NOT NULL,
     MName VARCHAR(50),
     LName VARCHAR(50) NOT NULL,
